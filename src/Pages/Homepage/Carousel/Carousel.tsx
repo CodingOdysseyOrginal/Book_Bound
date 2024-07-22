6ef536c4138c3de9e18ee21621e652f3887abf77
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2024-07-22 14:54:15 +0100

    Slidebar done

--- src/Pages/Homepage/Carousel/Carousel.tsx
+++ src/Pages/Homepage/Carousel/Carousel.tsx
@@ -6,14 +6,22 @@ import './Carousel.css';
 import CrackCode from '../imgs/CrackTheInterview.webp';
 import Dune from '../imgs/Dune.webp';
 import WillSaveTheGalaxyForCash from "../imgs/WillDestoryTheGalaxyForCash.jpg";
+import ACourtOfThornsAndRoses from "../imgs/ACourtOfThornsAndRoses.jpg"
+import KingsOfTheWild from "../imgs/KingsOfTheWyld.jpg";
+import ManKind from "../imgs/ManKind.jpg";
+import PrideAndPrejudice from "../imgs/PrideAndPrejudice.webp";
 
 const CarouselComponent: React.FC = () => {
   return (
     <Carousel 
-      infiniteLoop 
-      showThumbs={false} 
-      showStatus={false}
-    >
+    infiniteLoop 
+    showThumbs={false} 
+    showStatus={false}
+    autoPlay 
+    interval={5000} 
+    transitionTime={500} 
+    stopOnHover={true} 
+  >
       <div className='HomePageSlide1'>
         <Link to="#" className='Slide1Image'> 
           <img src={CrackCode} alt="Crack the Interview" />
@@ -39,7 +47,26 @@ const CarouselComponent: React.FC = () => {
       </div>
 
       <div className='HomePageSlide3'>
-        <p className="legend">Slide 3</p>
+        <div className='Slide3Left'>
+          <Link to="/Book_Bound/fantasy">
+            <img src={ACourtOfThornsAndRoses} alt="A Court of Thorns and Roses - Fantasy novel" />
+          </Link>
+          <Link to="/Book_Bound/fantasy">
+            <img src={KingsOfTheWild} alt="Kings of the Wild - Fantasy novel" />
+          </Link>
+        </div>
+        <div className='Slide3Center'>
+          <h1>See what we have to offer</h1>
+        </div>
+        <div className='Slide3Right'>
+          <Link to="/Book_Bound/scifi">
+            <img src={ManKind} alt="ManKind - Science fiction novel" />
+          </Link>
+          <Link to="/Book_Bound/classics">
+            <img src={PrideAndPrejudice} alt="Pride and Prejudice - Classic novel" />
+          </Link>
+        </div>
+        
       </div>
     </Carousel>
   );
