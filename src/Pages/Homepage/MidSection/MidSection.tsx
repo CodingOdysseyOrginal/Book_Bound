a29860e336ac4eaf943a29c1e1f125c1ef7c59d6
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2024-08-06 13:08:47 +0100

    button link added

--- src/Pages/Homepage/MidSection/MidSection.tsx
+++ src/Pages/Homepage/MidSection/MidSection.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import BookItem from '../../../Components/BookDesign/BookDesign';
 import books from '../../../Components/Data/HomePageBook';
 import './MidSection.css'; 
+import { Link } from 'react-router-dom';
 
 const MidSection: React.FC = () => {
   return (
     <div className='MidSectionContainer'>
       <div className='MidSectionInfo'>
       <h1><i>Everyone's Talking About...</i></h1>
+      <Link  to="/Book_Bound/bestsellers">
       <button>Bestsellers</button>
+      </Link>
       </div>
       <div className="bookList"> 
         {books.map((book) => (
